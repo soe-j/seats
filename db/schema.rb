3587ca74466c451d23c50fafb64fafcf73b97918
soe-j <soeda@val.co.jp>
2015-06-13 10:05:33 +0900

    make Group model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610005847) do
+ActiveRecord::Schema.define(version: 20150613005538) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "groups", ["name"], name: "index_groups_on_name", unique: true
 
   create_table "members", force: :cascade do |t|
     t.string   "name",       null: false
